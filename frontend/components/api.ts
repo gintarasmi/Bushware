7b257b2fecbde1955cf19bc96565dfbee8377f92
Justas Masiulis <justas.masiulis@yahoo.com>
2021-05-26 00:13:52 +0300

    API class fixes

--- frontend/components/api.ts
+++ frontend/components/api.ts
@@ -2,217 +2,154 @@ import jwtDecode from "jwt-decode";
 
 const apiUrl = "https://localhost:5001/api";
 
-export async function fetchJson(
-
-  url: string,
-  data: any,
-  options: any
-): Promise<Response> {
-  let res = await fetch(url, {
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-    ...options,
-  });
-  if (!res.ok) throw new Error(await res.text());
-  return res;
-}
-
 class Api {
-  _token: string;
-  _userId: string;
-  _role: string | string[];
+	_token: string;
+	_roles: string | string[];
 
-  constructor() {
-    if (typeof window !== "undefined") {
-      this._token = window.localStorage.getItem("token");
-    }
-  }
+	constructor() {
+		if (typeof window !== "undefined") {
+			this._setToken(window.localStorage.getItem("token"));
+		}
+	}
 
-  async login(email: string, password: string, isCourier: boolean): Promise<void> {
-    let res = await fetchJson(
-      `${apiUrl}/Auth/Login`,
-      { email, password, isCourier},
-      { method: "POST" }
-    );
-    this._token = await res.text();
-    window.localStorage.setItem("token", this._token);
-    let jwt = jwtDecode(this._token);
-    this._userId = (jwt as any).sub;
-    this._role = (jwt as any).role;
+	async login(email: string, password: string, isCourier: boolean): Promise<void> {
+		const res = await this._fetch(
+			`Auth/Login`,
+			{ email, password, isCourier },
+			{ method: "POST" }
+		);
 
-    console.log(this._role);
-  }
+		this._setToken(await res.text());
+		window.localStorage.setItem("token", this._token);
+	}
 
-  async register(
-        name: string,
-        email: string,
-        password: string,
-        isCourier: boolean
-    ): Promise<any> {
-        await fetchJson(
-            `${apiUrl}/Auth/Register`,
-            { name, email, password, isCourier },
-            { method: "POST" }
-        );
-    }
+	async register(
+		name: string,
+		email: string,
+		password: string,
+		isCourier: boolean
+	): Promise<void> {
+		await this._fetch(
+			`Auth/Register`,
+			{ name, email, password, isCourier },
+			{ method: "POST" }
+		);
+	}
+
+	logout() {
+		this._setToken(null);
+		window.localStorage.removeItem("token");
+	}
+
+	hasRole(role: string): boolean {
+		if (!this.signedIn) return false;
+
+		if (typeof this._roles === "string" || this._roles instanceof String)
+			return this._roles === role;
+		else return !!this._roles.find((r) => r === role);
+	}
+
+	get signedIn() {
+		return this._token !== null && this._token !== undefined;
+	}
 
 	async getOrders(): Promise<any> {
-		if (!this.signedIn) throw new Error("Not logged in");
-		let res = await fetch(`${apiUrl}/Orders/MyOrders`, {
-			headers: {
-				Authorization: `Bearer ${this._token}`,
-			},
-		});
-		return await res.json();
+		const res = await this._fetch(`Orders/MyOrders`);
+		return res.json();
 	}
 
-	async postOrder(data): Promise<any> {
-		if (!this.signedIn) throw new Error("Not logged in");
-		let res = await fetch(`${apiUrl}/Orders`, {
-			method: "POST",
-			headers: {
-				Authorization: `Bearer ${this._token}`,
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify(data),
-		});
-		return res;
-	}
-
-	async postCustOrder(data): Promise<any> {
-		if (!this.signedIn) throw new Error("Not logged in");
-		let res = await fetch(`${apiUrl}/CustOrder`, {
-			method: "POST",
-			headers: {
-				Authorization: `Bearer ${this._token}`,
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify(data),
-		});
-		return res;
+	async postCustOrder(data: any): Promise<void> {
+		await this._fetch(`CustOrder`, data, { method: "POST" });
 	}
 
 	async getCourierOrders(): Promise<any> {
-		if (!this.signedIn) throw new Error("Not logged in");
-		let res = await fetch(`${apiUrl}/Orders/MyShipments`, {
-			headers: {
-				Authorization: `Bearer ${this._token}`,
-			},
-		});
-		return await res.json();
+		const res = await this._fetch(`Orders/MyShipments`);
+		return res.json();
 	}
 
-	async acceptOrder(id: number): Promise<any> {
-		if (!this.signedIn) throw new Error("Not logged in");
-		let res = await fetch(`${apiUrl}/Orders/AcceptOrder/${id}`, {
-			method: "PUT",
-			headers: {
-				Authorization: `Bearer ${this._token}`,
-			},
-		});
-		return res;
+	async acceptOrder(id: number): Promise<void> {
+		await this._fetch(`Orders/AcceptOrder/${id}`, undefined, { method: "PUT" });
 	}
 
-	async pickedUpDelivery(id: number): Promise<any> {
-		if (!this.signedIn) throw new Error("Not logged in");
-		let res = await fetch(`${apiUrl}/Orders/PickedUpOrder/${id}`, {
-			method: "PUT",
-			headers: {
-				Authorization: `Bearer ${this._token}`,
-			},
-		});
-		return res;
+	async pickedUpDelivery(id: number): Promise<void> {
+		await this._fetch(`Orders/PickedUpOrder/${id}`, undefined, { method: "PUT" });
 	}
 
-	async deliveredShipment(id: number): Promise<any> {
-		if (!this.signedIn) throw new Error("Not logged in");
-		let res = await fetch(`${apiUrl}/Orders/DeliveredOrder/${id}`, {
-			method: "PUT",
-			headers: {
-				Authorization: `Bearer ${this._token}`,
-			},
-		});
-		return res;
+	async deliveredShipment(id: number): Promise<void> {
+		await this._fetch(`Orders/DeliveredOrder/${id}`, undefined, { method: "PUT" });
 	}
 
 	async getServices(): Promise<any> {
-		let res = await fetch(`${apiUrl}/Services`);
-		return await res.json();
+		const res = await this._fetch(`Services`);
+		return res.json();
 	}
 
-  async getAllOrders(): Promise<any> {
-    if (!this.signedIn) throw new Error("Not logged in");
-    let res = await fetch(`${apiUrl}/Orders`, {
-      headers: {
-        Authorization: `Bearer ${this._token}`,
-      },
-    });
-    return await res.json();
-  }
-  async getAllCustomers(): Promise<any> {
-    if (!this.signedIn) throw new Error("Not logged in");
-    let res = await fetch(`${apiUrl}/Customers`, {
-      headers: {
-        Authorization: `Bearer ${this._token}`,
-      },
-    });
-    return await res.json();
-  }
-  async getAllCouriers(): Promise<any> {
-    if (!this.signedIn) throw new Error("Not logged in");
-    let res = await fetch(`${apiUrl}/Couriers`, {
-      headers: {
-        Authorization: `Bearer ${this._token}`,
-      },
-    });
-    return await res.json();
-  }
+	async getAllOrders(): Promise<any> {
+		const res = await this._fetch(`Orders`);
+		return res.json();
+	}
+
+	async getAllCustomers(): Promise<any> {
+		const res = await this._fetch(`Customers`);
+		return res.json();
+	}
+
+	async getAllCouriers(): Promise<any> {
+		const res = await this._fetch(`Couriers`);
+		return res.json();
+	}
 
 	async getPayment(): Promise<any> {
-		let res = await fetch(`${apiUrl}/Payment`);
-		return await res.json();
+		const res = await this._fetch(`Payment`);
+		return res.json();
 	}
 
 	async getAddresses(): Promise<any> {
-		let res = await fetch(`${apiUrl}/custAddress/MyAddresses`, {
-			headers: {
-				Authorization: `Bearer ${this._token}`,
-			},
-		});
-		return await res.json();
+		const res = await this._fetch(`custAddress/MyAddresses`);
+		return res.json();
 	}
 
-	async postAddress(data): Promise<any> {
-		let res = await fetch(`${apiUrl}/CustAddress`, {
-			method: "POST",
+	async postAddress(data: any): Promise<void> {
+		await this._fetch(`CustAddress`, data, { method: "POST" });
+	}
+
+	async deleteCustomer(id: string): Promise<void> {
+		await this._fetch(`Customers/` + id, { id }, { method: "DELETE" });
+	}
+
+	async deleteCourier(id: string): Promise<void> {
+		await this._fetch(`Couriers/` + id, { id }, { method: "DELETE" });
+	}
+
+	_setToken(token: string | null) {
+		this._token = token;
+		if (token) {
+			const decoded = jwtDecode(this._token);
+			this._roles = (decoded as any).role;
+		}
+	}
+
+	async _fetch(url: string, data?: any, options?: any): Promise<Response> {
+		options = {
 			headers: {
-				Authorization: `Bearer ${this._token}`,
 				"Content-Type": "application/json",
 			},
 			body: JSON.stringify(data),
-		});
+			...options,
+		};
+
+		if (data) options.body = JSON.stringify(data);
+
+		if (this.signedIn) options.headers.Authorization = `Bearer ${this._token}`;
+
+		let res = await fetch(`${apiUrl}/${url}`, options);
+
+		if (!res.ok) {
+			console.error(res);
+			throw new Error(await res.text());
+		}
 		return res;
 	}
-
-	async deleteCustomer(id: string): Promise<any> {
-		await fetchJson(`${apiUrl}/Customers/` + id, { id }, { method: "DELETE" });
-	}
-
-  async deleteCourier(id: string): Promise<any> {
-		await fetchJson(`${apiUrl}/Couriers/` + id, { id }, { method: "DELETE" });
-	}
-
-  logout() {
-    this._token = undefined;
-    this._userId = undefined;
-    window.localStorage.removeItem("token");
-  }
-
-  get signedIn() {
-    return this._token !== undefined;
-  }
 }
 
 export let api = new Api();
